729bc0d017b826ead94946fcad1d5addc9e5ea28
Mugada Jai Krishna Chaitanya <jaikrishnamugada@gmail.com>
2025-05-11 22:09:27 +0530

    details

---
+++ script.js
@@ -0,0 +1,9 @@
+const inputBox = document.getElementById('input-box');
+const SearchBtn = document.getElementById('search-button');
+const weatherImage = document.querySelector('weather-img');
+const temperature = document.querySelector('temperature');
+const description = document.querySelector('description');
+const humidity = document.getElementById('humidity');
+const windSpeed = document.getElementById('wind_speed');
+
+
