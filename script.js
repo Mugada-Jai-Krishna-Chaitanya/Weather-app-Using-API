a1263a239b3be83b1ea60fc08835a6e9f345d6da
Mugada Jai Krishna Chaitanya <jaikrishnamugada@gmail.com>
2025-05-14 22:24:41 +0530

    API connection

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const inputBox = document.getElementById('input-box');
-const SearchBtn = document.getElementById('search-button');
+const searchBtn = document.getElementById('search-button');
 const weatherImage = document.querySelector('weather-img');
 const temperature = document.querySelector('temperature');
 const description = document.querySelector('description');
 const humidity = document.getElementById('humidity');
 const windSpeed = document.getElementById('wind_speed');
 
+async function checkWeather(city){
+    const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
+    // const api_key = "7d6b37a2b38b0ec1f9239642cce1c3b6";
+    const api_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
+    const weather_data =await fetch(`${url}`).then(response => response.json());
+
+    console.log(weather_data);
+}
+
+
+searchBtn.addEventListener('click',()=>{
+    checkWeather(inputBox.value);
+});
